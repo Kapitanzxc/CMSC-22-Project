6ddebe7f046266a233ba7f95aaa05364b6af36d2
Kapitanzxc <vinlyt08@gmail.com>
2024-05-06 12:24:28 +0800

    Added Menu
    Menu including new game (not working), about, and developers

---
+++ CMSCProject/src/elements/ControlScene.java
@@ -0,0 +1,112 @@
+package elements;
+
+import java.util.ArrayList;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class ControlScene {
+//	Attributes
+	private Group root;
+	private Stage stage;
+	private Scene controlScene;
+	private Scene parentScene;
+	private Scene menuScene;
+	private Canvas canvas;
+	private GraphicsContext gc;
+	
+	// Constructor
+	public ControlScene(Scene menuScene, Scene parentScene, Stage stage) {
+		// Initialize variables
+		this.stage = stage;
+		this.root = new Group();
+		this.controlScene = new Scene(this.root,Formatting.SCREEN_WIDTH, Formatting.SCREEN_HEIGHT);
+		this.menuScene = menuScene;
+		this.parentScene = parentScene;
+		this.canvas = new Canvas(Formatting.SCREEN_WIDTH,Formatting.SCREEN_HEIGHT);
+		this.gc = canvas.getGraphicsContext2D();
+		
+		// Draw Background
+		this.gc.drawImage(Formatting.CONTROLS, 0, 0, Formatting.SCREEN_WIDTH,Formatting.SCREEN_HEIGHT);
+//		Draw Scroll Wheel
+		this.gc.drawImage(Formatting.MIDDLE_SCROLL, (Formatting.SCREEN_WIDTH - Formatting.LEFT_SCROLL.getWidth())/2, Formatting.YPOS_TEXT);
+		
+		// Draw right button
+		ImageView imgView = new ImageView(Formatting.RIGHT_BUTTON);
+		Button rightButton = new Button();
+		rightButton.setPadding(Insets.EMPTY);
+		rightButton.setTranslateX(Formatting.XPOS_TEXT+68);
+		rightButton.setTranslateY(Formatting.YPOS_TEXT);
+		rightButton.setPrefSize(Formatting.RIGHT_BUTTON.getWidth(),Formatting.RIGHT_BUTTON.getHeight() );
+		rightButton.setGraphic(imgView);
+		rightButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+//			Go to Reference Scene
+			public void handle(MouseEvent event) {
+				System.out.println("Load Reference Scene");
+				ReferenceScene referenceScene = new ReferenceScene(menuScene, controlScene, stage);
+				stage.setScene(referenceScene.getScene());
+			}	
+			
+		});
+	    
+	    // Draw left button
+		ImageView imgView1 = new ImageView(Formatting.LEFT_BUTTON);
+		Button leftButton = new Button();
+		leftButton.setPadding(Insets.EMPTY);
+		leftButton.setTranslateX(Formatting.XPOS_TEXT-100);
+		leftButton.setTranslateY(Formatting.YPOS_TEXT);
+		leftButton.setPrefSize(Formatting.LEFT_BUTTON.getWidth(),Formatting.LEFT_BUTTON.getHeight() );
+		leftButton.setGraphic(imgView1);
+		leftButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+//			Go to About Scene
+			public void handle(MouseEvent event) {
+				// Go back to the parent Scene
+				System.out.println("Load About Scene");
+				stage.setScene(parentScene);
+			}	
+			
+		});
+		
+//		// Draw home button
+		ImageView imgViewHome = new ImageView(Formatting.HOME_BUTTON);
+		Button homeButton = new Button();
+		homeButton.setPadding(new Insets(0));
+		homeButton.setTranslateX(7);
+		homeButton.setTranslateY(7);
+		homeButton.setPrefSize(Formatting.HOME_BUTTON.getWidth(),Formatting.HOME_BUTTON.getHeight() );
+		homeButton.setGraphic(imgViewHome);
+		homeButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				// Go to Menu Scene
+				System.out.println("Load Menu Scene");
+				stage.setScene(menuScene);
+			}	
+			
+		});
+	   
+		// Load these things
+		this.root.getChildren().add(this.canvas);
+		this.root.getChildren().add(rightButton);
+		this.root.getChildren().add(leftButton);
+		this.root.getChildren().add(homeButton);
+
+	}
+	
+	public Scene getScene() {
+		return controlScene;
+	}
+	
+	
+}
---
+++ CMSCProject/src/elements/SplashScreen.java
@@ -0,0 +1,125 @@
+package elements;
+
+import java.awt.Font;
+import java.util.ArrayList;
+
+import javafx.event.EventHandler;
+//import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.FontWeight;
+//import javafx.scene.input.MouseEvent;
+//import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+public class SplashScreen {
+//	Attributes
+	private Stage stage;
+	private Group root;
+	private Scene splashScene;
+	private Canvas canvas;
+	private GraphicsContext gc;
+	private Image titlePicture;
+	private ArrayList<Text> texts;
+	
+	public static final double XPOS_TEXT = Formatting.SCREEN_WIDTH/2-100;
+	public static final double YPOS_TEXT = Formatting.SCREEN_HEIGHT/2+130;
+	
+	//constructor
+	public SplashScreen(Stage stage) {
+		this.stage = stage;
+		this.root = new Group();
+		this.splashScene = new Scene(this.root,Formatting.SCREEN_WIDTH, Formatting.SCREEN_HEIGHT);
+		this.canvas = new Canvas(Formatting.SCREEN_WIDTH, Formatting.SCREEN_HEIGHT);
+		this.gc = canvas.getGraphicsContext2D();
+	 	this.titlePicture = new Image("file:resources/gameLogo.png", 800, 800 , true, true); 
+	 	this.texts = new ArrayList<Text>();
+	}
+//	//sets stage
+	public void setStage() {
+        // Draw the images on the canvas
+        this.gc.drawImage(Formatting.BACKGROUND, 0, 0, Formatting.SCREEN_WIDTH, Formatting.SCREEN_HEIGHT);
+        this.gc.drawImage(this.titlePicture, (Formatting.SCREEN_WIDTH - titlePicture.getWidth())/2, 10); 
+        //texts
+  		texts.add(new Text(XPOS_TEXT, YPOS_TEXT,"NEW GAME"));
+  		texts.add(new Text(XPOS_TEXT, YPOS_TEXT+35,"ABOUT"));
+  		texts.add(new Text(XPOS_TEXT, YPOS_TEXT+70,"DEVELOPERS"));
+  		//customizes texts and adds events
+  		for(Text t: texts) {
+  			t.setFont(Formatting.FONT_STYLE_26);
+  			t.setFill(Color.WHITE);
+  			t.setStroke(Color.BLACK);
+  			this.addMouseEvent(t);
+  		}
+
+        // Add the canvas and texts to the root node
+        this.root.getChildren().add(this.canvas);
+        this.root.getChildren().add(texts.get(0));
+		this.root.getChildren().add(texts.get(1));
+		this.root.getChildren().add(texts.get(2));
+		this.stage.setTitle("CMSC 22 Project: Grow and Conquer");
+		this.stage.setScene(this.splashScene);
+		this.stage.show();
+	}
+	
+	//creates mouse events for texts
+	private void addMouseEvent(Text t) {
+		//changes color when text is hovered in
+		t.setOnMouseEntered(new EventHandler<MouseEvent>(){
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				t.setFill(Color.LIGHTGREEN);
+			}
+		});
+		//changes color when text is hovered out
+		t.setOnMouseExited(new EventHandler<MouseEvent>(){
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				t.setFill(Color.WHITE);
+			}
+		});
+		//changes scene when text is clicked
+		t.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				// when new game is clicked
+				if(t.getText().equals("NEW GAME")) {
+					System.out.println("Load New Game Scene");
+					//change scene here
+//					GameTimer gameScene = new GameTimer(splashScene, stage);
+//					stage.setScene(gameScene.getScene());
+				}
+				//when about is clicked
+				else if(t.getText().equals("ABOUT")) {
+					System.out.println("Load About Scene");
+//					change scene here
+					AboutScene aboutScene = new AboutScene(splashScene, stage);
+					stage.setScene(aboutScene.getScene());
+				}
+				//when developers is clicked
+				else {
+					System.out.println("Load Developer Scene");
+//					change scene here
+					DeveloperScene developerScene = new DeveloperScene(splashScene, stage);
+					stage.setScene(developerScene.getScene());
+				}
+			}
+			
+		});
+			
+//		
+	}
+	
+
+}
