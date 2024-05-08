169064c0f4baaa766b29914da6b4f93f3923e478
Kapitanzxc <vinlyt08@gmail.com>
2024-05-08 09:24:36 +0800

    Added two player
    - With bugs
          - Player 1 stops when Player 2 stops and vice versa

---
+++ CMSCProject/src/elements/GameTimer.java
@@ -0,0 +1,120 @@
+package elements;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GameTimer extends AnimationTimer {
+
+	private Scene gameScene;
+	private GraphicsContext gc;
+	private Knight knight;
+	private Knight pixel;
+	private long previousTime;
+	private Knight knight2;
+	private long previousTime2;
+	
+	GameTimer(GraphicsContext gc, Scene theScene) {
+		this.gc = gc;
+		this.previousTime = System.nanoTime();
+		this.gameScene = theScene;
+//		Two knights
+		this.knight = new Knight(344,100,2);
+		this.knight2 = new Knight(370,100,2);
+		//call method to handle mouse click event
+		this.handleKeyPressEvent();
+	}
+	
+	@Override
+	public void handle(long currentNanoTime) {
+		// TODO Auto-generated method stub
+		this.gc.clearRect(0, 0, Formatting.SCREEN_WIDTH, Formatting.SCREEN_HEIGHT);
+		this.gc.drawImage(Formatting.MAP, 0, 0, Formatting.SCREEN_WIDTH,Formatting.SCREEN_HEIGHT);
+		// Method to move the two players
+		this.knight.move();
+		this.knight2.move();
+//		Method to show idle animation
+		this.previousTime = this.knight.idleStart(System.nanoTime(), this.previousTime);
+		this.previousTime2 = this.knight2.idleStart(System.nanoTime(), this.previousTime2);
+//		Spawning two knights
+		this.knight.render(this.gc);
+		this.knight2.render(this.gc);
+
+	}
+	
+	//method that will listen and handle the key press events
+	private void handleKeyPressEvent() {
+		gameScene.setOnKeyPressed(new EventHandler<KeyEvent>(){
+			public void handle(KeyEvent e){
+            	KeyCode code = e.getCode();
+                moveCharacter(code);
+			}
+			
+		});
+		gameScene.setOnKeyReleased(new EventHandler<KeyEvent>(){
+		            public void handle(KeyEvent e){
+		            	KeyCode code = e.getCode();
+		                stopCharacter(code);
+		        		
+		            }
+		        });
+    }
+	
+	//method that will move the knight depending on the key pressed
+	private void moveCharacter(KeyCode ke) {
+		if(ke==KeyCode.UP) {
+			this.knight.setDY(-3);                 
+		}
+		if(ke==KeyCode.LEFT) {
+			this.knight.setDX(-3);
+		}
+		if(ke==KeyCode.DOWN) {
+			this.knight.setDY(3);
+		}
+		if(ke==KeyCode.RIGHT) {
+			this.knight.setDX(3);
+		}
+		if(ke==KeyCode.W) {
+			this.knight2.setDY(-3);                 
+		}
+		if(ke==KeyCode.A) {
+			this.knight2.setDX(-3);
+		}
+		if(ke==KeyCode.S) {
+			this.knight2.setDY(3);
+		}
+		if(ke==KeyCode.D) {
+			this.knight2.setDX(3);
+		}
+			
+		
+		System.out.println(ke+" key pressed.");
+   	}
+	
+	//method that will stop the ship's movement; set the ship's DX and DY to 0
+	private void stopCharacter(KeyCode ke){
+		this.knight.setDX(0);
+		this.knight.setDY(0);
+		this.knight2.setDX(0);
+		this.knight2.setDY(0);
+	}
+	
+	public Scene getScene() {
+		return gameScene;
+	}
+
+}
